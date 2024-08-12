049ade7ea9120836b59a3dfb9ede0a2bcc3eff22
Joshua Patton <jkpatton75@icloud.com>
2024-08-11 22:33:50 -0700

    edit src

--- challenge/Develop/server/src/routes/api/weatherRoutes.ts
+++ challenge/Develop/server/src/routes/api/weatherRoutes.ts
@@ -1,19 +1,43 @@
 import { Router, type Request, type Response } from 'express';
+import WeatherService from '../../service/weatherService.js';
 const router = Router();
 
 // import HistoryService from '../../service/historyService.js';
 // import WeatherService from '../../service/weatherService.js';
 
 // TODO: POST Request with city name to retrieve weather data
-router.post('/', (req: Request, res: Response) => {
-  // TODO: GET weather data from city name
-  // TODO: save city to search history
+router.post('/', async (req: Request, res: Response) => {
+  const { city } = req.body;
+  try {
+    await HistoryService.saveCity(city);
+    res.sendStatus(200);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to save city to search history' });
+  }
+
 });
 
 // TODO: GET search history
-router.get('/history', async (req: Request, res: Response) => {});
+router.get('/history', async (req: Request, res: Response) => {
+  try {
+    const history = await HistoryService.getSearchHistory();
+    res.status(200).json(history);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to retrieve search history' });
+  }
+});
+router.get('/history', async (req: Request, res: Response) => { });
 
 // * BONUS TODO: DELETE city from search history
-router.delete('/history/:id', async (req: Request, res: Response) => {});
+router.delete('/history/:id', async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    await HistoryService.deleteCity(id);
+    res.sendStatus(200);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to delete city from search history' });
+  }
+});
+router.delete('/history/:id', async (req: Request, res: Response) => { });
 
 export default router;

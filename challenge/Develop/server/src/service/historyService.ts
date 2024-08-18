dd0b05ad432731cc8c6d7867d083492f4e939d30
Joshua Patton <jkpatton75@icloud.com>
2024-08-17 19:26:51 -0700

    update historyService.ts

--- challenge/Develop/server/src/service/historyService.ts
+++ challenge/Develop/server/src/service/historyService.ts
@@ -5,25 +5,25 @@
 
 // TODO: Complete the HistoryService class
 class HistoryService {
-  saveCity(city: any) {
+  saveCity(_city: any) {
     throw new Error('Method not implemented.');
   }
   getSearchHistory() {
     throw new Error('Method not implemented.');
   }
-  deleteCity(id: string) {
+  deleteCity(_id: string) {
     throw new Error('Method not implemented.');
   }
   // TODO: Define a read method that reads from the searchHistory.json file
-  // private async read() {}
-  // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
-  // private async write(cities: City[]) {}
+  async read() {
+    // Remove the duplicate function declaration here
+  }
   // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
-  // async getCities() {}
+  async getCities() { }
   // TODO Define an addCity method that adds a city to the searchHistory.json file
-  // async addCity(city: string) {}
+  async addCity() { }
   // * BONUS TODO: Define a removeCity method that removes a city from the searchHistory.json file
-  // async removeCity(id: string) {}
+  async removeCity(_id: string) { }
 }
 
 export default new HistoryService();
